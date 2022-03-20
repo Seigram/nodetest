8b23791798fb3e82be1e44e314bc4337fb222d83
Seigram <seigram0884@gmail.com>
2022-03-21 07:02:20 +0900

    app get modify

--- app.js
+++ app.js
@@ -101,6 +101,4 @@ app.use((err, req, res, next) => {
     res.status(err.status || 500).render('error');
 });
 
-app.listen(app.get('port'), () => {
-    console.log(app.get('port'), ' 번 포트 대기');
-});
+module.exports = app
